0c349fe4b2b8b8004fa012371e0bb5dfebb8544d
sajandangal <42007423+sajandangal@users.noreply.github.com>
2020-07-30 19:26:16 +0545

    Friends User List

---
+++ app/src/main/java/com/stafftracking/project/chatblo/Friends.java
@@ -0,0 +1,25 @@
+package com.social.joanlouji.chatblo;
+/**
+ * Created by chill_group
+ * agile computing project android Devlopment
+ */
+public class Friends {
+
+    public String date;
+
+    public Friends(){
+
+    }
+
+    public Friends(String date) {
+        this.date = date;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+}
