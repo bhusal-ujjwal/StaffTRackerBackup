25d11a6c9c1b8815170e0eb9bdb4b1d4bc82c0ef
akibmdhr <akib.manandhar10@gmail.com>
2020-08-07 15:29:04 +0545

    conversation code inititated.

---
+++ app/src/main/java/com/stafftracking/project/chatblo/Conv.java
@@ -0,0 +1,61 @@
+package com.stafftracking.project.chatblo;
+
+/**
+ * Created by chill_group
+ * agile computing project android Devlopment
+ */
+
+public class Conv {
+
+    public boolean seen;
+    public long timestamp;
+
+    public Conv(){
+
+    }
+
+    public boolean isSeen() {
+        return seen;
+    }
+
+    public void setSeen(boolean seen) {
+        this.seen = seen;
+    }
+
+    public long getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(long timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public Conv(boolean seen, long timestamp) {
+        this.seen = seen;
+        this.timestamp = timestamp;
+    }
+}
+/*
+*2020-06-02
+* 2020-06-08
+* 2020-06-09
+* 2020-06-15
+* 2020-06-16
+* 2020-06-22
+* 2020-06-23
+* 2020-06-29
+* 2020-06-30
+* 2020-07-06
+* 2020-07-07
+* 2020-07-13
+* 2020-07-14
+* 2020-07-20
+* 2020-07-21
+* 2020-07-31
+*
+*
+*
+*
+*
+*
+* */
