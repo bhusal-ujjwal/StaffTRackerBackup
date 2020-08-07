9d1eaba22e40fa1f58c013318505b65ae3f73fd8
swikriti123 <46412889+swikriti123@users.noreply.github.com>
2020-08-07 15:47:57 +0545

    ProfileS test

--- app/src/androidTest/java/com/stafftracking/project/chatblo/ProfileSettingMenu.java
+++ app/src/androidTest/java/com/stafftracking/project/chatblo/ProfileSettingMenu.java
@@ -4,9 +4,10 @@ import androidx.test.rule.ActivityTestRule;
 
 import org.junit.Rule;
 import org.junit.Test;
+//import
 
-import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
 
