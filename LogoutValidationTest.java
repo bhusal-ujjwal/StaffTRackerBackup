43ae4a2bff4014b299e12d192e292e78e26043e0
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-08-20 11:29:03 +0545

    Add files via upload

---
+++ LogoutValidationTest.java
@@ -0,0 +1,20 @@
+package com.stafftracking.project.chatblo;
+
+
+import androidx.test.rule.ActivityTestRule;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+public class LogoutValidationTest {
+    @Rule
+    public ActivityTestRule<MainActivity> activity_logoutActivityTestRule = new ActivityTestRule<>(MainActivity.class);
+    @Test
+    public void LogoutValidationTest() {
+        onView(withId(R.id.logouticon)).perform(click());
+    }
+}
