9b2946c5eb9185840c606fd7f9992dbbca065241
Ujjwal Bhusal <45995959+bhusalujjwal@users.noreply.github.com>
2020-08-20 13:39:15 +0545

    some test cases

---
+++ app/src/androidTest/java/com/stafftracking/project/chatblo/assets/feature/spalsh.feature
@@ -0,0 +1,8 @@
+@splash-feature
+Feature: Splash Screen
+
+  @spalsh-feature
+  Scenario: Launch Tracker App with Tracker Logo
+    Given I am on application drawer
+    When I click on tracker application
+    Then I should see spalsh screen with app logo
---
+++ app/src/androidTest/java/com/stafftracking/project/chatblo/test/RegisterActivitySteps.java
@@ -0,0 +1,63 @@
+package com.stafftracking.project.chatblo.test;
+
+import android.content.Intent;
+
+import androidx.test.espresso.Espresso;
+import androidx.test.rule.ActivityTestRule;
+
+import com.stafftracking.project.chatblo.R;
+import com.stafftracking.project.chatblo.RegisterActivity;
+
+import org.junit.Rule;
+
+import cucumber.api.java.After;
+import cucumber.api.java.Before;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static junit.framework.TestCase.assertNotNull;
+
+//import static androidx.test.espresso.assertion.ViewAssertions.matches;
+//import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
+
+public class RegisterActivitySteps {
+
+    @Rule
+    private ActivityTestRule<RegisterActivity> activityTestRule = new ActivityTestRule<>(RegisterActivity.class);
+
+    private RegisterActivity activity;
+
+    @Before("@signup-feature")
+    public void setUp() {
+        activityTestRule.launchActivity(new Intent());
+        activity = activityTestRule.getActivity();
+    }
+
+    @After("@signup-feature")
+    public void tearDown() {
+        activityTestRule.finishActivity();
+    }
+
+    @Given("^I am on register screen$")
+    public void I_am_on_register_screen() {
+        assertNotNull(activity);
+    }
+
+    @When("^I tap register button$")
+    public void I_tap_register_button() {
+//        Close the keyboard else the login button is not available for click on the screen
+        Espresso.closeSoftKeyboard();
+        onView(withId(R.id.reg_create_btn)).perform(click());
+    }
+
+    @Then("^I should see dashboard screen$")
+    public void I_should_see_dashboard_screen() {
+        onView(withId(R.id.dashboard));
+//        onView(withId(R.id.dashboard)).check(matches(withText(R.string.login)));
+    }
+}
